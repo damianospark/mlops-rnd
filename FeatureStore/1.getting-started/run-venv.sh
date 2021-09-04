e6e7f2ce86d3c169994ab3800c2580958b32281b
Damianos Park <damianospark@gmail.com>
2021-09-05 02:39:58 +0900

    complte getting started

--- FeatureStore/1.getting-started/run-venv.sh
+++ FeatureStore/1.getting-started/run-venv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 env_name=$1
-cmd="$2 $3 $4 $5 $6 $7 $8"
+cmd="$2 $3 $4 $5 $6 $7 $8 $9"
 __conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 
 . /opt/conda/etc/profile.d/conda.sh
