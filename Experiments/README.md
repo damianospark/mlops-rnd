103a4572de8947294d2d26b926a22372ff137b07
Damianos Park <damianospark@gmail.com>
2021-09-07 03:47:33 +0900

    mlflow initial study

---
+++ Experiments/README.md
@@ -0,0 +1,5 @@
+# MLflow
+
+1. install on local environment
+
+2. Install on GCP
---
+++ Experiments/install-venv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# to influence current shell execute this script with source
+# $ source install-venv.sh
+
+
+# yes | jupyter kernelspec remove 
+env_nm=mlflow
+
+conda init --all
+conda deactivate
+conda create -n $env_nm  python=3.7
+conda env list
+conda activate $env_nm
+conda install -n $env_nm nb_conda_kernels
+#jupyter kernelspec install feast-conda-env
+
+conda install -n $env_nm  pip ipykernel
+python -m ipykernel install --user --name $env_nm --display-name "$env_nm env"
+jupyter kernelspec list
+conda env list
+ls /opt/conda/envs/$env_nm
+
+# pip uninstall luigi
+pip install mlflow[extras] Pygments -U
